150ed98ea4cbad972f140150154c3497a34fcc44
Alexander Veselov <a.veselov83@gmail.com>
2020-11-25 20:43:13 +0300

    document-structure homework task 1 code fixed - to close active tooltip by click

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -5,10 +5,14 @@ const tooltip = document.createElement('div');
 tooltip.classList.add('tooltip');
 
 function showTooltip(event) {
-    tooltip.classList.add('tooltip_active');
-    tooltip.innerText = event.target.title;
-    event.target.appendChild(tooltip);
-    tooltip.style.left = `${event.target.getBoundingClientRect().left}px`;
+    if (event.target.contains(document.querySelector('.tooltip_active'))) {
+        tooltip.classList.remove('tooltip_active');
+    } else {
+        tooltip.classList.add('tooltip_active');
+        tooltip.innerText = event.target.title;
+        event.target.appendChild(tooltip);
+        tooltip.style.left = `${event.target.getBoundingClientRect().left}px`;
+    }
 }
 
 hasTooltip.forEach(element => {
